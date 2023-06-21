ea773ed7f771456ab8ea80a547c1db4f06e6a3b6
billwk254 <billkihungu51@gmail.com>
2023-06-21 13:56:46 +0300

    Function that calculates the modulus of the second number in the stack in relation to the first

--- execute.c
+++ execute.c
@@ -18,7 +18,7 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 			  {"sub", stk_subtraction},
 			  {"div", stk_division},
 			  {"mul", stk_multiply},
-			  {"mod", f_mod},
+			  {"mod", stk_modulus},
 			  {"pchar", f_pchar},
 			  {"pstr", f_pstr},
 			  {"rotl", f_rotl},
