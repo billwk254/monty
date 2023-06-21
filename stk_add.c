a1da159982de4c0c0f317d8eb65169964d5ab60f
billwk254 <billkihungu51@gmail.com>
2023-06-21 12:57:56 +0300

    Monty program with ability to print the stack,push elements to the stack,print the value at the top of the stack

---
+++ stk_add.c
@@ -0,0 +1,93 @@
+#include "monty.h"
+/**
+ * stk_add - Perfoms an addition on the top two elements of a stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: nothing
+ */
+void stk_add(stack_t **head, unsigned int counter)
+{
+stack_t *h;
+int len = 0, aux;
+
+h = *head;
+while (h)
+{
+h = h->next;
+len++;
+}
+if (len < 2)
+{
+fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+h = *head;
+aux = h->n + h->next->n;
+h->next->n = aux;
+*head = h->next;
+free(h);
+}
+
+/**
+ * addnode - Node added to the head stack
+ * @head: stack head
+ * @n: new_value
+ * Return: nothing
+ */
+void addnode(stack_t **head, int n)
+{
+
+stack_t *new_node, *aux;
+
+aux = *head;
+new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+printf("Error\n");
+exit(0);
+}
+if (aux)
+aux->prev = new_node;
+new_node->n = n;
+new_node->next = *head;
+new_node->prev = NULL;
+*head = new_node;
+}
+
+/**
+ * addqueue - add node to the tail stack
+ * @n: new_value
+ * @head: head of the stack
+ * Return: no return
+ */
+void addqueue(stack_t **head, int n)
+{
+stack_t *new_node, *aux;
+
+aux = *head;
+new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+printf("Error\n");
+}
+new_node->n = n;
+new_node->next = NULL;
+if (aux)
+{
+while (aux->next)
+aux = aux->next;
+}
+if (!aux)
+{
+*head = new_node;
+new_node->prev = NULL;
+}
+else
+{
+aux->next = new_node;
+new_node->prev = aux;
+}
+}
---
+++ stk_queue.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * stk_queue - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: nothing
+ */
+void stk_queue(stack_t **head, unsigned int counter)
+{
+(void)head;
+(void)counter;
+bus.lifi = 1;
+}
